ca0c03a1b5144c468293cd39477688585c522878
Alex Grigorenko <agrigorenko@mail.greenriver.edu>
2020-01-17 13:20:32 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// Turn on error reporting -- this is critical!
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require("vendor/autoload.php");
+
+//Instantiate F3
+$f3 = Base::Instance();
+
+//Define a default route
+$f3->route("GET /", function (){
+    //$view = new Template();
+    //echo $view->render("Pet.html");
+    echo "Pet Home";
+});
+
+//Run f3
+$f3->run();
